d7cf86c02ddd57b8015ccc4d547f80d0906ad099
AMDLnghrns <aduncan001@gmail.com>
2019-06-13 17:56:14 -0500

    Updates

---
+++ precache-manifest.719e71bc37252520c7141665468dde59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc110535216caa36b44343e18f02d8c6",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "6c4b8d3eb25424d9455f",
+    "url": "/clicky/static/css/main.bf6193f9.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6c4b8d3eb25424d9455f",
+    "url": "/clicky/static/js/main.c1c261ec.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  }
+]);
